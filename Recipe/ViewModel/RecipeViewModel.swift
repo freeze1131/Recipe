8533f615d30d6dc71d9675984a53e284beacb169
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-07 11:20:14 +0300

    added view model for recipes

---
+++ Recipe/ViewModel/RecipeViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  RecipeViewModel.swift
+//  Recipe
+//
+//  Created by Ahmet Ozen on 7.02.2024.
+//
+
+import SwiftUI
+
+
+@Observable class RecipeViewModel {
+    var recipeModels:[RecipeModel] = []
+    
+    //Initialize the model
+    init(){
+        recipeModels.append(
+            RecipeModel(name: "Hummus",
+                        caloriesPer100Grams: 322,
+                        recipe: """
+                                        **Ingredients**
+                                        -...
+                                        -...
+                                        -...
+                                        -...
+                                        """,
+                        image: "IMGhummus",
+                        recipeURL: "https://www.inspiredtaste.net/15938/easy-and-smooth-hummus-recipe/")
+            )
+        recipeModels.append(
+            
+            RecipeModel(name: "Homemade Pizza",
+                        caloriesPer100Grams: 166,
+                        recipe: """
+                                        **Ingredients**
+                                        -...
+                                        -...
+                                        -...
+                                        -...
+                                        """,
+                        image: "IMGPizza",
+                        recipeURL: "https://www.simplyrecipes.com/recipes/homemade_pizza/"))
+            
+        recipeModels.append(
+            RecipeModel(name: "Apple Pie",
+                        caloriesPer100Grams: 237,
+                        recipe: """
+                                        **Ingredients**
+                                        -...
+                                        -...
+                                        -...
+                                        -...
+                                        """,
+                        image: "IMGApplePie",
+                        recipeURL: "https://www.allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/"))
+    }
+    
+}
