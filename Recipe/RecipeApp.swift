4410e8877e7dfddcacdc699f93ba288ed0334f47
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-07 11:07:22 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Ahmet Ozen on 7.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
